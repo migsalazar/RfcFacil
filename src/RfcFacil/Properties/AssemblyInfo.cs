41052dce9d6306563576203205281e4b72aba2e9
Miguel Salazar <mig.salazars@hotmail.com>
2016-06-12 19:53:30 -0500

    update info , nuget configs, add tools

--- src/RfcFacil/Properties/AssemblyInfo.cs
+++ src/RfcFacil/Properties/AssemblyInfo.cs
@@ -1,36 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("RfcFacil")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Librería para el cálculo del RFC del SAT en México")]
 [assembly: AssemblyCompany("Miguel Salazar")]
-[assembly: AssemblyProduct("RfcFacil")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4fa9a703-5c04-4f7f-a01e-90719407d54b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyProduct("RfcFacil.NET")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
